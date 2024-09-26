183b3edbabbe4c213b6d5d553894e984cfcfaadd
YuchenDou <23009960>
2024-09-26 23:12:39 +0800

    1.0.0

---
+++ src/main/java/org/example/Function_Manage.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Function_Manage {
+    GUI gui;
+
+    public Function_Manage(GUI gui) {
+
+        this.gui = gui;
+    }
+
+    public void CutText() {
+        // Cut the selected text and remove it from the text area
+        gui.textArea.cut();
+    }
+
+    public void CopyText() {
+        // Copy the selected text to the clipboard
+        gui.textArea.copy();
+    }
+
+    public void PasteText() {
+        // Paste text from the clipboard into the text area
+        gui.textArea.paste();
+    }
+
+    public void SelectAll() {
+        // Select all text in the text area
+        gui.textArea.selectAll();
+    }
+}
