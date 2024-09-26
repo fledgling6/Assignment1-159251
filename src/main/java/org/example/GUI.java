3e6507e5618279b7d0b21e79b6aede662a3df25f
YuchenDou <23009960>
2024-09-26 23:33:15 +0800

    1.0.0

--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -226,7 +226,6 @@ public class GUI implements ActionListener {
             case "Copy":manage.CopyText();break;
             case "Paste":manage.PasteText();break;
             case "SelectAll":manage.SelectAll();break;
-
         }
     }
 }
