688affbb4085df36aa105119aeeba1a230de2f8b
YuchenDou <23009960>
2024-09-29 21:11:03 +0800

    pml changed

--- src/main/java/nz/ac/massey/cs251/GUI.java
+++ src/main/java/nz/ac/massey/cs251/GUI.java
@@ -8,7 +8,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class GUI implements ActionListener {
     JFrame room;
 
